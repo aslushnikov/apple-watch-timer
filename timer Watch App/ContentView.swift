3776499eef2324ac6f5de3a20d568ecec8541ab1
Andrey Lushnikov <aslushnikov@gmail.com>
2024-05-18 10:47:31 -0700

    Initial Commit

---
+++ timer Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  timer Watch App
+//
+//  Created by Andrey Lushnikov on 5/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ timer Watch App/timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  timerApp.swift
+//  timer Watch App
+//
+//  Created by Andrey Lushnikov on 5/18/24.
+//
+
+import SwiftUI
+
+@main
+struct timer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

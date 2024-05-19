f480f1dd95d0bc7b2c8a71a2fe97f0d697c17b0d
Andrey Lushnikov <aslushnikov@gmail.com>
2024-05-19 15:39:41 -0700

    feat: support background operation

--- timer Watch App/ContentView.swift
+++ timer Watch App/ContentView.swift
@@ -6,17 +6,20 @@
 //
 
 import SwiftUI
+import HealthKit
 
 struct ContentView: View {
     @State private var timerRunning = false
     @State private var timeElapsed = 0.0
     @State private var timer: Timer?
     @State private var selectedInterval = 10
+    @State private var healthStore = HKHealthStore()
+    @State private var workoutSession: HKWorkoutSession?
     
     let intervals = [5, 10, 15, 30, 60]
     
     var body: some View {
-
+        
         VStack {
             
             Text(timeString(time: timeElapsed))
@@ -40,10 +43,14 @@ struct ContentView: View {
             
         }
         .padding()
+        .onAppear {
+            requestHealthKitAuthorization()
+        }
     }
     
     func startTimer() {
         timerRunning = true
+        startWorkoutSession()
         timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
             timeElapsed += 1.0
             if Int(timeElapsed) % selectedInterval == 0 {
@@ -57,6 +64,7 @@ struct ContentView: View {
         timer = nil
         timerRunning = false
         timeElapsed = 0.0
+        endWorkoutSession()
     }
     
     func timeString(time: Double) -> String {
@@ -68,6 +76,36 @@ struct ContentView: View {
     
     func triggerHaptic() {
         WKInterfaceDevice.current().play(.notification)
+    }
+    
+    func requestHealthKitAuthorization() {
+        let healthKitTypes: Set = [
+            HKObjectType.workoutType()
+        ]
+        
+        healthStore.requestAuthorization(toShare: healthKitTypes, read: healthKitTypes) { (success, error) in
+            if !success {
+                print("HealthKit authorization denied!")
+            }
+        }
+    }
+    
+    func startWorkoutSession() {
+        let workoutConfiguration = HKWorkoutConfiguration()
+        workoutConfiguration.activityType = .other
+        workoutConfiguration.locationType = .unknown
+
+        do {
+            workoutSession = try HKWorkoutSession(healthStore: healthStore, configuration: workoutConfiguration)
+            workoutSession?.startActivity(with: Date())
+        } catch {
+            print("Failed to start workout session: \(error.localizedDescription)")
+        }
+    }
+    
+    func endWorkoutSession() {
+        workoutSession?.end()
+        workoutSession = nil
     }
 }
 
